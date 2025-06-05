a0067a9a5b694a4aa1bf93879cf18d50badf5667
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-05 15:41:40 +0700

    menambahkan int push int value

--- stack.cpp
+++ stack.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 class Node
@@ -15,12 +16,23 @@ public:
 //Stack class
 class Stack 
 {
-Private:
+private:
     Node *top;//pointer to the top node of the stack
 
 public:
     Stack()
     {
-        top = NULL; // Initialize the stack with a nuul top
+        top = NULL; // Initialize the stack with a null top pointer
+    }
+
+    //push oporation: insert an element the top of the stack
+    int push(int value) 
+    {
+        Node* newNode = new Node(); //1. Allocate memory for a new node
+        newNode->data = value;      //2. assign value
+        newNode->next = top;        //3. set the next pointer of the new node to the current
+        top = newNode;              //4. update the top pointer to point to the new node
+        cout << "Push value: " << value << endl; //
+        return value;
     }
 }
