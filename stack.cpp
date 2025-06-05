f5e5591b6ff11f67f83053c73eb4251eb1077b0e
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-05 15:55:44 +0700

    menambahkan case 1 didalam switch choice

--- stack.cpp
+++ stack.cpp
@@ -91,4 +91,13 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
     }
+
+    switch (choice) 
+        {
+        case 1:
+            cout << "Enter value to push: ";
+            cin >> value;
+            stack.push(value); //Push the entered value onto the stack
+            break;
+        }
 }
